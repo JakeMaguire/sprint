c70acf79c8c645a7673862daee64fb33fb14068e
Jake Maguire <jake@themaguires.co.uk>
2024-10-20 16:44:02 +0100

    Added first version of routine page

--- src/app/routines/page.tsx
+++ src/app/routines/page.tsx
@@ -1,3 +1,88 @@
+import Image from "next/image";
+import skincare from "../../../public/skincare.jpg";
+
 export default function Routines() {
-  return <main></main>;
+  return (
+    <main>
+      <div className="mt-16 text-center">
+        <h1>Cleansers</h1>
+        <h4>Description about cleansers</h4>
+      </div>
+
+      <div className="mt-16">
+        <h4 className="mb-2">Popular Routines</h4>
+        <div className="grid grid-cols-5 gap-8">
+          <div>
+            <Image src={skincare} alt="" />
+            <p className="font-semibold tracking-tight mt-2">
+              Morning Acne Routine
+            </p>
+            <p className="text-sm text-muted-foreground">Jake Maguire</p>
+          </div>
+          <div>
+            <Image src={skincare} alt="" />
+          </div>
+          <div>
+            <Image src={skincare} alt="" />
+          </div>
+          <div>
+            <Image src={skincare} alt="" />
+          </div>
+          <div>
+            <Image src={skincare} alt="" />
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-16">
+        <h4 className="mb-2">New Routines</h4>
+        <div className="grid grid-cols-5 gap-8">
+          <div>
+            <Image src={skincare} alt="" />
+            <p className="font-semibold tracking-tight mt-2">
+              Morning Acne Routine
+            </p>
+            <p className="text-sm text-muted-foreground">Jake Maguire</p>
+          </div>
+          <div>
+            <Image src={skincare} alt="" />
+          </div>
+          <div>
+            <Image src={skincare} alt="" />
+          </div>
+          <div>
+            <Image src={skincare} alt="" />
+          </div>
+          <div>
+            <Image src={skincare} alt="" />
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-16">
+        <h4 className="mb-2">Just Commented</h4>
+        <div className="grid grid-cols-5 gap-8">
+          <div>
+            <Image src={skincare} alt="" />
+            <p className="font-semibold tracking-tight mt-2">
+              Morning Acne Routine
+            </p>
+            <p className="text-sm text-muted-foreground">Jake Maguire</p>
+          </div>
+          <div>
+            <Image src={skincare} alt="" />
+          </div>
+          <div>
+            <Image src={skincare} alt="" />
+          </div>
+          <div>
+            <Image src={skincare} alt="" />
+          </div>
+          <div>
+            <Image src={skincare} alt="" />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
